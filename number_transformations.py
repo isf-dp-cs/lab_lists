7aed9e1c474600c32a81415c9debb50bb89caf0f
ms.brown <eqbrown@isf.edu.hk>
2025-10-13 15:00:29 +0800

    solutions

--- number_transformations.py
+++ number_transformations.py
@@ -3,20 +3,31 @@
 
 def total_sum(num_list):
     '''Returns sum of numbers in num_list'''
-
-    return 
+    sum = 0
+    for num in num_list:
+        sum += num
+    return sum
  
 
 def minimum(num_list):
     '''Returns the smallest number in num_list'''
+    min = num_list[0]
+    for num in num_list:
+        if num < min:
+            min = num
 
-    return 
+    return min
 
 
 def maximum(num_list):
     '''Returns the smallest number in num_list'''
+    
+    max = num_list[0]
+    for num in num_list:
+        if num > max:
+            max = num
 
-    return 
+    return max 
 
 def biggest_difference(num_list):
     '''Given an array length 1 or more of ints, 
@@ -24,11 +35,20 @@ def biggest_difference(num_list):
 
         Which previous function(s) may be helpful?
     '''
-
-    return
+    
+    min = minimum(num_list)
+    max = maximum(num_list)
+    return max - min
 
 
 if __name__ == '__main__':
     # TODO: write 2 tests for each function using asert 
-    
     assert total_sum([2,4,6]) == 12
+
+    assert minimum([2,4,6]) == 2
+
+    assert maximum([2,4,6]) == 6
+
+    assert biggest_difference([2,4,6]) == 4
+
+
--- word_transformations.py
+++ word_transformations.py
@@ -2,50 +2,80 @@
 
 def get_word_list(file):
     """Returns a list of words without the newline charachter"""
-    
-    return 
+
+    file = open(file,'r')
+    word_list = []
+    for line in file:
+        word_list.append(line.strip())
+    file.close()
+    return word_list
 
 def get_words_of_length(word_list, length):
     '''Returns a list of words of a given length'''
-
-    return
+    words = []
+    for word in word_list:
+        if len(word) == length:
+            words.append(word)
+    return words
 
 def get_words_including(word_list, including_string):
     '''Returns a list of words including the string anywhere in the word'''
-
-    return
+    words = []
+    for word in word_list:
+        if including_string in word:
+            words.append(word)
+    return words
 
 
 def get_words_starting(word_list, starting_string):
     '''Returns a list of words that include the string at the start of the word'''
-
-    return
+    words = []
+    for word in word_list:
+        if starting_string == word[0:len(starting_string)]:
+            words.append(word)
+    return words
 
 def get_words_ending(word_list, ending_string):
     '''Returns a list of words that include the string at the end of the word'''
-
-    return
+    words = []
+    for word in word_list:
+        length = len(word)
+        start = length - len(ending_string)
+        if ending_string == word[start:]:
+            words.append(word)
+    return words
 
 
 if __name__ == '__main__':
-    # TODO 1) What is the longest word?
+
+    word_list = get_word_list("words_100k.txt")
+
+    # TODO 1) What is the distrubtion of number of words for all lengths of words in the list? 
+    print("\n--TODO 1")
+
+    for i in range(19):
+        print(i, len(get_words_of_length(word_list,i)))
 
 
-    print()
-
-    # TODO 2) Which words contain the word "ability"? 
-
-
-    print()
-
-    # TODO 3) How many words have the word "cat" in them?
+    # TODO 2) What is the longest word?
+    print("\n--TODO 2")
+    print(get_words_of_length(word_list,18))
 
 
-    print()
+    # TODO 3) Which words contain the word "ability"? 
+    print("\n--TODO 3")
+    print(get_words_including(word_list,'ability'))
 
-    # TODO 4) Which words contain double 'o's and double 's's. 
+
+    # TODO 4) Which 10 letter words contain the word "cat" in them?
+    print("\n--TODO 4")
+    print(get_words_of_length(get_words_including(word_list,'cat'),10))
+
+    # TODO 5) Which words contain double 'o's and double 's's. 
+    print("\n--TODO 5")
+    print(get_words_including(get_words_including(word_list,'oo'),'ss'))
 
 
-    print()
-
+    print("\n--TODO 6")
     # TODO 5) Which words have all five vowels (aeiou)?
+    print(get_words_including(get_words_including(get_words_including(get_words_including(get_words_including(word_list,'a'),'e'),'i'),'o'),'u'))
